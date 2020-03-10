e90861a3de1f7dc2b20f711b2dc667bbfc847094
Barrytech <barryabdoul2024@gmail.com>
2020-03-10 11:15:56 -0400

    damn

--- src/index.js
+++ src/index.js
@@ -11,12 +11,3 @@ ReactDOM.render( < App / > , document.getElementById('root'));
 // Learn more about service workers: https://bit.ly/CRA-PWA
 
 serviceWorker.unregister();
-
-componentDidMount() {
-    const script = document.createElement("script");
-    script.src = "/static/public/interact.js";
-    script.async = true;
-    script.onload = () => this.scriptLoaded();
-
-    document.body.appendChild(script);
-}
